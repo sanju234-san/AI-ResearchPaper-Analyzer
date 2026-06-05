c692ad3517ed234cbd13104240ec3ce129f9bbef
Sanjeevni Dhir <sanjeevnidhir05@gmail.com>
2026-06-05 14:42:52 +0530

    fix: resolve medium security issue in client/src/main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
